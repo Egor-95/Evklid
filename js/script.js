f59ff71ac6b4704e22b098003578cfb3e8436ab4
Egor Sporysh <egorsporush13791995@gmail.com>
2022-10-06 15:42:37 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// hero-swiper
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+});
+// work-tabs
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.work__btn').forEach (function (e) {
+e.addEventListener ('click',function (e) {
+  const tab = e.currentTarget.dataset.path
+  document.querySelectorAll('.work__btn').forEach ( elem => {
+  elem.classList.remove ('work__btn__active')
+  })
+  this.classList.add('work__btn__active')
+  document.querySelectorAll('.tab-content').forEach (function (e){
+    e.classList.remove('tab-content--active')
+    document.querySelector(`[data-target='${tab}']`).classList.add('tab-content--active')
+  })
+})
+  })
+})
+// question-accordion
+$( function() {
+  $( "#accordion" ).accordion();
+} );
+$( "#accordion" ).accordion({
+  collapsible: true,
+  active: false,
+  heightStyle: 'panel'
+});
